b079210dc9a2c16176cf3d9910a2b93095c64a78
Krzychu159 <krzysztof.rak159@gmail.com>
2025-05-05 18:42:05 +0200

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,58 @@
 import "./Footer.scss";
-import { ReactComponent as Logo } from "../assets/logo.svg";
+import logo from "../assets/logo.svg";
+import fb from "../assets/icon-facebook.svg";
+import tw from "../assets/icon-twitter.svg";
+import pin from "../assets/icon-pinterest.svg";
+import insta from "../assets/icon-instagram.svg";
 
 function Footer() {
   return (
     <footer>
-      <div className="logo-container">
-        <Logo className="logo" />
-      </div>
-      <div className="menu">
-        <div className=""></div>
+      <div className="content">
+        <div className="logo-container">
+          <img src={logo} alt="" />
+        </div>
+        <div className="menu">
+          <div>
+            <div className="tittle">Feature</div>
+            <div>
+              {" "}
+              <ul>
+                <li>Link Shortening</li>
+                <li>Branded Links</li>
+                <li>Analytics</li>
+              </ul>{" "}
+            </div>
+          </div>
+          <div>
+            <div className="tittle">Feature</div>
+            <div>
+              {" "}
+              <ul>
+                <li>Link Shortening</li>
+                <li>Branded Links</li>
+                <li>Analytics</li>
+              </ul>{" "}
+            </div>
+          </div>
+          <div>
+            <div className="tittle">Feature</div>
+            <div>
+              {" "}
+              <ul>
+                <li>Link Shortening</li>
+                <li>Branded Links</li>
+                <li>Analytics</li>
+              </ul>{" "}
+            </div>
+          </div>
+        </div>
+        <div className="social-media">
+          <img src={fb} alt="" />
+          <img src={tw} alt="" />
+          <img src={pin} alt="" />
+          <img src={insta} alt="" />
+        </div>
       </div>
     </footer>
   );
