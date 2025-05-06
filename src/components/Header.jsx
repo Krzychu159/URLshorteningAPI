357ed964ab05e13e8f6c28c8e3883aaf63cb230e
Krzychu159 <krzysztof.rak159@gmail.com>
2025-05-06 19:30:30 +0200

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,22 @@ import logo from "../assets/logo.svg";
 import "./Header.scss";
 
 function Header() {
+  const toggleMenu = () => {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+  };
+
   return (
     <header>
       <div className="left">
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
+
+        <div className="hamburger" onClick={toggleMenu}>
+          &#9776;
+        </div>
+
         <div className="menu">
           <ul>
             <li>Features</li>
@@ -16,7 +26,8 @@ function Header() {
           </ul>
         </div>
       </div>
-      <div>
+
+      <div className="buttons">
         <button className="login">Login</button>
         <button>Sign Up</button>
       </div>
